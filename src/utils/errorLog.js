becdc80b02c841fb8d4b7e883b42de047803f041
yulin Liu <122687220@qq.com>
2019-02-24 22:03:04 +0800

    优化错误日志

--- src/utils/errorLog.js
+++ src/utils/errorLog.js
@@ -1,21 +1,120 @@
 import Vue from 'vue'
+import { updateLog } from '@/api/errorLog'
 
+let debugConfig = {
+    //项目名称
+    entryName: 'entryName',
+    //脚本版本
+    scriptVersion: '1.0',
+    // 环境
+    releaseStage: 'pro'
+  },
+  debug = {
+    notifyWarn({ message, metaData }) {
+      let type = 'caught',
+        severity = 'warn'
+
+      _logReport({ type, severity, message, metaData })
+    },
+    notifyError({ type = 'caught', error, message, metaData }) {
+      let severity = 'error'
+
+      _logReport({ type, severity, error, metaData, message })
+    }
+  }
+
+// 日志上报
+function _logReport({ type, severity, error, metaData, message }) {
+  message = message || (error && error.message) || ''
+
+  //这里可以做一个灰度控制
+
+  let { entryName, releaseStage, scriptVersion } = debugConfig,
+    name = (error && error.name) || 'error',
+    stacktrace = (error && error.stack) || '',
+    time = Date.now(),
+    title = document.title,
+    url = window.location.href,
+    client = {
+      userAgent: window.navigator.userAgent,
+      height: window.screen.height,
+      width: window.screen.width,
+      referrer: window.document.referrer
+    },
+    pageLevel = 'p0'
+
+  //此处可以给你的页面进行分级
+  //getPageLevel();
+
+  //通过接口上传错误日志，需后台实现接口
+  updateLog({
+    entryName,
+    scriptVersion,
+    message,
+    metaData,
+    name,
+    releaseStage,
+    severity,
+    stacktrace,
+    time,
+    title,
+    type,
+    url,
+    client,
+    pageLevel //页面等级
+  })
+}
+
+function formatComponentName(vm) {
+  if (vm.$root === vm) return 'root'
+  let name = vm._isVue
+    ? (vm.$options && vm.$options.name) ||
+      (vm.$options && vm.$options._componentTag)
+    : vm.name
+  return (
+    (name ? 'component <' + name + '>' : 'anonymous component') +
+    (vm._isVue && vm.$options && vm.$options.__file
+      ? ' at ' + (vm.$options && vm.$options.__file)
+      : '')
+  )
+}
+
+//如果是生产环境捕获错误信息并上传
 if (process.env.NODE_ENV === 'production') {
-  // vm 为抛出异常的Vue实例
-  // info 为 Vue 特定的错误信息，比如错误所在的生命周期钩子
   Vue.config.errorHandler = function(err, vm, info) {
-    // Don't ask me why I use Vue.nextTick, it just a hack.
-    // detail see https://forum.vuejs.org/t/dispatch-in-vue-config-errorhandler-has-some-problem/23500
-    let {
-      message, // 异常信息
-      name, // 异常名称
-      stack // 异常堆栈信息
-    } = err
-    Vue.nextTick(() => {
-      const errInfo = `${message}${name}${stack}`
-      console.error(errInfo)
-      console.error(vm)
-      console.error(info)
+    if (vm) {
+      let componentName = formatComponentName(vm)
+      let propsData = vm.$options && vm.$options.propsData
+      debug.notifyError({
+        error: err,
+        metaData: {
+          componentName,
+          propsData,
+          info,
+          userToken: { token: 'user-token' } //metaData可以存一些额外数据，比如：用户信息等
+        }
+      })
+    } else {
+      debug.notifyError({
+        error: err,
+        metaData: {
+          userToken: { token: 'user-token' } //metaData可以存一些额外数据，比如：用户信息等
+        }
+      })
+    }
+  }
+
+  window.onerror = function(msg, url, lineNo, columnNo, error) {
+    debug.notifyError({
+      type: 'uncaught',
+      error,
+      metaData: {
+        userToken: { token: 'user-token' } //metaData可以存一些额外数据，比如：用户信息等
+      },
+      message: msg,
+      lineNumber: lineNo,
+      columnNumber: columnNo,
+      fileName: url
     })
   }
 }
